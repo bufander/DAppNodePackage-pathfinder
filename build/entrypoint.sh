fd56a31eb4a2c2a2a7256d7b851350db612f3968
bufander <bufanderbufander@gmail.com>
2022-06-28 06:48:55 +0200

    Updated upstream version

--- build/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo  "Starting Starknet node"
-echo "Ethereum L1 node http(s) url: $PATHFINDER_ETHEREUM_API_URL"
-
-# Arguments are passed through env variables: https://github.com/eqlabs/pathfinder#running-with-docker
-tini -- /usr/local/bin/pathfinder
